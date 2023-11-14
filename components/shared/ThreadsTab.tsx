bd4b8f2d63628f1f89b76a5ecb06d0a9f038b96f
Wojtek Mitan <wojtekmitan91@gmail.com>
2023-11-14 19:29:29 +0100

    profile page

---
+++ components/shared/ThreadsTab.tsx
@@ -0,0 +1,10 @@
+interface Props {
+  currentUserId: string;
+  accountId: string;
+  accountType: string;
+}
+
+const ThreadsTab = async ({ currentUserId, accountId, accountType }: Props) => {
+  return <section>ThreadsTab</section>;
+};
+export default ThreadsTab;
--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -3,6 +3,7 @@
 import { revalidatePath } from "next/cache";
 import User from "../models/user.model";
 import { connectToDB } from "../mongoose";
+import Thread from "../models/thread.model";
 
 interface Params {
   userId: string;
@@ -57,4 +58,24 @@ export async function fetchUser(userId: string) {
   } catch (error: any) {
     throw new Error(`Failed to fetch user: ${error.message}`);
   }
+}
+
+export async function fetchUserPosts(userId: string) {
+  try {
+    connectToDB();
+
+    const threads = await User.findOne({ id: userId }).populate({
+      path: "threads",
+      model: Thread,
+      populate: {
+        path: "children",
+        model: Thread,
+        populate: {
+          path: "author",
+          model: User,
+          select: "name image id",
+        },
+      },
+    });
+  } catch (error: any) {}
 }
